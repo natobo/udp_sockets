8c22d8a270e34f3389eedd0121e43896fa7de9f3
Nicolás Tobo <na.tobo@uniandes.edu.co>
2020-10-25 09:56:35 -0500

    Modificando el TCP a UDP

---
+++ udp_transfer_file/MultiClientHash_Udp.py
@@ -0,0 +1,117 @@
+import socket
+import time
+from threading import Thread
+import struct
+import hashlib
+import datetime
+import sys
+
+# Constante que representa la ip del servidor con el que los clientes debe comunicarse
+TCP_IP = '34.71.37.77'
+# Constante que representa el puerto por el que se debe hacer la conexión ip
+TCP_PORT = 65432
+# Constante que representa el número de bytes que se envían por segmento en el buffer
+BUFFER_SIZE = 1024
+# Constante que representa el mensaje de que se finalizo la transmisión de un mensaje
+END_TRANSMISION = b'TERMINO'
+
+
+# Método que permite verificar la integridad del archivo enviado
+def VerificateHash(originalHash, filename):
+    file = open(filename, 'rb')
+    md5_returned = hashlib.md5(file.read()).hexdigest()
+    if originalHash.decode() == md5_returned:
+        return "HASH VERIFICADO"
+    else:
+        return "HASH ALTERADO"
+
+
+""" Los siguientes métodos auxiliares se usaron para conformar los mensajes con protocolo TCP
+Fueron tomados de este blog: https://stupidpythonideas.blogspot.com/2013/05/sockets-are-byte-streams-not-message.html"""
+# Método auxiliar que cumple la funcion de recibir en su totalidad un paquete (contraparte del método sendall de socket)
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf:
+            return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+# Envía un mensaje que se compone de una estructura que indica la cantidad de bytes en su encabezado
+def send_one_message(sock, data):
+    length = len(data)
+    sock.sendall(struct.pack('!I', length))
+    sock.sendall(data)
+
+# Recibe un mensaje enviado por el servidor, interpretando la estructura de este (encabezado y bytes de archivo)
+def recv_one_message(sock):
+    lengthbuf = recvall(sock, 4)
+    length, = struct.unpack('!I', lengthbuf)
+    return recvall(sock, length)
+
+# Clase que genera un Thread de tipo cliente el cual genera un log único
+class ClientThread(Thread):
+    #Metodo que inicia un Thread
+    def __init__(self, id):
+        Thread.__init__(self)
+        print(" Nuevo thread en"+str(time.time())+":"+str(TCP_PORT))
+        self.id = id
+    #Metodo que inicia la ejecución del Thread Cliente
+    def run(self):
+        # Preparacion del log
+        LogTxt = 'log_cliente_' + \
+            str(self.id) + '_' + \
+            str(time.time()) + '.txt'
+        # Variables usadas por el log
+        tInicio = 0
+        tFinal = 0
+        numPaquetesRecibidos = 0
+        bytesRecibidos = 0
+        with open(LogTxt, 'w') as log:
+            log.write("Fecha y hora de la prueba: " +
+                      str(datetime.datetime.now()) + '\n')
+            # Ejecucion del programa
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect((TCP_IP, TCP_PORT))
+            tInicio = time.time_ns()
+            log.write("Tiempo de inicio de ejecucion de Th_" +
+                      str(self.id)+" : "+str(tInicio)+'\n')
+            recived_f = 'archivo' + \
+                str(self.id) + str(time.time()).split('.')[0] + '.mp4'
+
+            with open(recived_f, 'wb') as f:
+                print('Archivo abierto')
+                while True:
+                    data = recv_one_message(s)
+                    # print('data=%s', (data))
+                    if repr(data) == repr(END_TRANSMISION):
+                        f.close()
+                        print('file close()')
+                        tFinal = time.time_ns()
+                        log.write("Tiempo final de ejecucion de Th_"+str(self.id) + " : "
+                                  + str(tFinal) + '\n')
+                        log.write("Tiempo de ejecucion de Th_"+str(self.id)+" : "
+                                  + str((tFinal-tInicio)) + '\n')
+                        break
+                    # write data to a file
+                    f.write(data)
+                    numPaquetesRecibidos += 1
+                    bytesRecibidos += sys.getsizeof(data)
+                log.write("Numero paquetes recibidos de Th_" +
+                          str(self.id)+" : "+str(numPaquetesRecibidos) + '\n')
+                log.write("Bytes recibidos por Th_" +
+                          str(self.id)+" : "+str(bytesRecibidos) + '\n')
+                codigoVerificacion = recv_one_message(s)
+                rtaVerificacion = VerificateHash(codigoVerificacion, recived_f)
+                print('Resultado de validacion: ' + rtaVerificacion)
+                send_one_message(s, rtaVerificacion.encode())
+                send_one_message(
+                    s, (str(numPaquetesRecibidos)+";"+str(bytesRecibidos)).encode())
+            print('Archivo descargado con exito')
+            s.close()
+            print('Conexion cerrada')
+#Inicializa los Threads definidos por el range
+for i in range(1):
+    ClientThread(i).start()
---
+++ udp_transfer_file/MultiServerHash_Udp.py
@@ -0,0 +1,155 @@
+import socket
+from threading import Thread
+import struct
+import hashlib
+import time
+import sys
+import datetime
+TCP_IP = ''
+TCP_PORT = 65432
+BUFFER_SIZE = 1024
+END_TRANSMISION = b'TERMINO'
+
+# Variable que almacena el codigo md5 en hexadecimal del archivo a enviar
+Verification_code = 'NoCodigo'
+# Variables usadas por el log
+fileGlobal = ""
+
+#Crea un codigo de verificacion MD5 del archivo que se le pase por parametro y lo retorna
+def createVerificationCode(filename):
+    global Verification_code
+    if(Verification_code == 'NoCodigo'):
+        file = open(filename, 'rb')
+        Verification_code = hashlib.md5(file.read()).hexdigest()
+        print("Codigo de verificacion:"+Verification_code)
+    return Verification_code
+
+""" Los siguientes métodos auxiliares se usaron para conformar los mensajes con protocolo TCP
+Fueron tomados de este blog: https://stupidpythonideas.blogspot.com/2013/05/sockets-are-byte-streams-not-message.html"""
+# Método auxiliar que cumple la funcion de recibir en su totalidad un paquete (contraparte del método sendall de socket)
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf:
+            return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+# Envía un mensaje que se compone de una estructura que indica la cantidad de bytes en su encabezado
+def send_one_message(sock, data):
+    length = len(data)
+    sock.sendall(struct.pack('!I', length))
+    sock.sendall(data)
+
+# Recibe un mensaje enviado por el servidor, interpretando la estructura de este (encabezado y bytes de archivo)
+def recv_one_message(sock):
+    lengthbuf = recvall(sock, 4)
+    length, = struct.unpack('!I', lengthbuf)
+    return recvall(sock, length)
+
+# Clase que genera un Thread que representa un cliente al cual transmitirle los datos dentro del servidor
+class ClientThread(Thread):
+    # Contructor del Thread
+    def __init__(self, ip, port, sock, id):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.sock = sock
+        self.id = id
+        print(" Nuevo Thread comenzado por "+ip+":"+str(port))
+    #Metodo que ejecuta el thread
+    def run(self):
+        #Variables para la escritura del log
+        tInicio = 0
+        tFinal = 0
+        numPaquetesEnviados = 0
+        numPaquetesRecibidos = 0
+        bytesEnviados = 0
+        bytesRecibidos = 0
+        global fileGlobal
+        correctoGlobal = True
+        filename = fileGlobal
+        print('Filename'+filename)
+        f = open(filename, 'rb')
+        while True:
+            l = f.read(BUFFER_SIZE)
+            while (l):
+                numPaquetesEnviados += 1
+                bytesEnviados += sys.getsizeof(l)
+                send_one_message(self.sock, l)
+                l = f.read(BUFFER_SIZE)
+            if not l:
+                f.close()
+                print('Termino la transferencia')
+                break
+        print('Enviando Comando:', repr(END_TRANSMISION))
+        send_one_message(self.sock, END_TRANSMISION)
+        # Envia codigo de verificacion
+        send_one_message(self.sock, createVerificationCode(filename).encode())
+        # Recibe respuesta del cliente
+        print(self.sock)
+        rta = recv_one_message(self.sock).decode()
+        print(rta)
+        correctoGlobal &= rta == 'HASH VERIFICADO'
+        numPaquetesCliente, numBytesCliente = recv_one_message(
+            self.sock).decode().split(';')
+        print("num"+numPaquetesCliente)
+        numPaquetesRecibidos += int(numPaquetesCliente)
+        print("by"+numBytesCliente)
+        bytesRecibidos += int(numBytesCliente)
+        self.sock.close()
+        with open(LogTxt, 'w') as log:
+            tFinal = time.time_ns()
+            log.write("Tiempo final de ejecucion del th" +
+                      self.id + ": " + str(tFinal) + '\n')
+            log.write("Tiempo de ejecucion desde inicio de la prueba" +
+                      self.id + ": " + str((tFinal-tInicio)) + '\n')
+            log.write("Numero de paquetes enviados hasta el th" + self.id + ": " +
+                      str(numPaquetesEnviados) + "\n")
+            log.write("Numero de paquetes recibidos hasta el th" + self.id + ": " +
+                      str(numPaquetesRecibidos) + "\n")
+            log.write("Numero de bytes enviados hasta el th" +
+                      self.id + ": " + str(bytesEnviados) + "\n")
+            log.write("Numero de bytes recibidos hasta el th" + self.id + ": " +
+                      str(bytesRecibidos) + "\n")
+            log.write("Correctitud del envio hasta el th" +
+                      self.id + ": " + str(correctoGlobal) + "\n")
+            log.close()
+
+# Crea el socket UDP por el que el servidor estará escuchando a los clientes
+udpsock = s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udpsock.bind((TCP_IP, TCP_PORT))
+threads = []
+# Aplicacion de ejecucion del servidor para definir cual archivo es que el que va transmitir y a cuantos clientes.
+print("Hola!, bienvenido a la aplicacion del grupo 11, por favor selecciona el archivo de video a mandar: "+"\n")
+print("1. Video 1 de  100 MB"+"\n")
+print("2. Video 2 de  250 MB"+"\n")
+opcion = int(input("Ingresa una opción: "))
+fileGlobal = 'ventilador_100.mp4' if (opcion == 1) else 'hielo_250.mp4'
+print("Listo, menciona el numero de clientes a los que quieres antender en simultaneo para enviar el archivo: "+"\n")
+opcion2 = int(input("Ingresa el numero de clientes: "))
+# Preparacion del log
+LogTxt = 'log_servidor' + \
+    '_'+str(time.time()) + '.txt'
+with open(LogTxt, 'w') as log:
+    log.write("Fecha y hora de la prueba: " +
+              str(datetime.datetime.now()) + '\n')
+    log.close()
+clientId = 0
+#Loop infinito que acepta conexiones de clientes entrantes
+while True:
+    udpsock.listen(25)
+    print("Esperando por conexiones entrantes...")
+    (conn, (ip, port)) = udpsock.accept()
+    print('Conexion desde  ', (ip, port))
+    clientId += 1
+    newthread = ClientThread(ip, port, conn, clientId)
+    threads.append(newthread)
+    while len(threads) >= opcion2:
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+            threads.remove(t)
