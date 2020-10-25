4139722961a7e68d7eab9f245a36d9f9bdb65bdd
Nicolás Tobo <na.tobo@uniandes.edu.co>
2020-10-25 18:08:33 -0500

    agregando hash

--- udp_transfer_file/ClienteUdp.py
+++ udp_transfer_file/ClienteUdp.py
@@ -1,40 +1,50 @@
 import socket
 import sys
 import time
+import hashlib
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_address = ('34.71.37.77', 10000)
 buf=1024
+separador=','
+filename=''
+hashcode=''
+
+# Método que permite verificar la integridad del archivo enviado
+def VerificateHash(originalHash, filename):
+    file = open(filename, 'rb')
+    md5_returned = hashlib.md5(file.read()).hexdigest()
+    if originalHash.decode() == md5_returned:
+        return "HASH VERIFICADO"
+    else:
+        return "HASH ALTERADO"
 
 try:
     # Send data
     message1 = 'Hola servidor!'
     print('sending "%s"' % message1)
     sent = sock.sendto(message1.encode(), server_address)
-
     # Receive response
-    #print ('waiting to receive')
-    #data, server = sock.recvfrom(4096)
-    #print ('received "%s"' % data)
-    
-    filename,addr = sock.recvfrom(buf)
-    print ("Received File:",filename.decode())
-    
+    msg,addr = sock.recvfrom(buf)
+    print ("Msg recibido:",msg.decode())
+    filename = msg.decode().split(separador)[0]  
+    hashcode = msg.decode().split(separador)[1]   
     # Send data
     message2 = 'Estoy listo para recibir!'
     print('sending "%s"' % message2)
     sent = sock.sendto(message2.encode(), server_address)
-
+    
     f = open(filename.decode()+'_'+str(time.time()).split('.')[0]+'.jpg','wb')
     
     data,addr = sock.recvfrom(buf)
     while(data):
         f.write(data)
-        data,addr = sock.recvfrom(buf) 
-    
-    print('closing socket')
+        sock.settimeout(2)
+        data,addr = sock.recvfrom(buf)
+except:
     f.close()
-finally:
     sock.close()
+    print(VerificateHash(hashcode,filename))
+    print("File Downloaded")
--- udp_transfer_file/ServerUdp.py
+++ udp_transfer_file/ServerUdp.py
@@ -1,6 +1,6 @@
 import socket
 import sys
-
+import hashlib
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,21 +13,33 @@ sock.bind(server_address)
 file_name='dogs.jpg'
 buf =1024
 
+# Variable que almacena el codigo md5 en hexadecimal del archivo a enviar
+Verification_code = 'NoCodigo'
+separador = ','
+
+#Crea un codigo de verificacion MD5 del archivo que se le pase por parametro y lo retorna
+def createVerificationCode(filename):
+    global Verification_code
+    if(Verification_code == 'NoCodigo'):
+        file = open(filename, 'rb')
+        Verification_code = hashlib.md5(file.read()).hexdigest()
+        print("Codigo de verificacion:"+Verification_code)
+    return Verification_code
+
 while True:
-    print ('\nwaiting to receive message')
+    print ('\nWaiting to receive message')
     data, address = sock.recvfrom(buf)
     print ('received %s bytes from %s' % (len(data), address))
     print (data)
 
-    #Envia nombre del archivo
-    print("HII::"+file_name.encode())
-    sock.sendto(file_name.encode(),address)
+    #Envia nombre del archivo y el codigo de verificacion de Hash
+    print("HII::"+file_name+separador+createVerificationCode(file_name))
+    sock.sendto(file_name+separador+createVerificationCode(file_name).encode(),address)
     
     print ('\nEsperando Confirmacion de cliente')
     ack, address = sock.recvfrom(buf)
     print ('received %s bytes from %s' % (len(data), address))
     print (ack)
-    
     
     if ack:
         f=open(file_name,"rb")
