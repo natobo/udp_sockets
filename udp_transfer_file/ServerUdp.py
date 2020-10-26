f2beb3eccb30bb20a2311f1da5422a1e7b945aad
Nicolás Tobo <na.tobo@uniandes.edu.co>
2020-10-25 20:10:01 -0500

    ojala no mueras con el log 2

--- udp_transfer_file/ServerUdp.py
+++ udp_transfer_file/ServerUdp.py
@@ -19,7 +19,7 @@ print("Hola!, bienvenido a la aplicacion de transferencia de archivos UDP del gr
 print("1. Video 1 de  100 MB"+"\n")
 print("2. Video 2 de  250 MB"+"\n")
 print("3. Imagen jpg de perritos"+"\n")
-opcion = int(input("Ingresa una opción: "))
+opcion = int(input("Ingresa una opcion: "))
 file_name = 'ventilador_100.mp4' if (opcion == 1) else 'hielo_250.mp4' if (opcion == 2) else 'dogs.jpg'
 print("Listo, menciona el numero de clientes a los que quieres antender en simultaneo para enviar el archivo: "+"\n")
 opcion2 = int(input("Ingresa el numero de clientes: "))
