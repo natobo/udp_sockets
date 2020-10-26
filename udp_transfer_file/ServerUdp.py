0840c509616e0d2fac9adc5f362701eb49fa690d
Nicolás Tobo <na.tobo@uniandes.edu.co>
2020-10-25 21:44:12 -0500

    Bendiciones x1000

--- udp_transfer_file/ServerUdp.py
+++ udp_transfer_file/ServerUdp.py
@@ -3,8 +3,64 @@ import sys
 import hashlib
 import time
 import datetime
+from threading import Thread
 
-# Create a TCP/IP socket
+# Clase que genera un Thread que representa un cliente al cual transmitirle los datos dentro del servidor
+class ClientThread(Thread):
+    # Contructor del Thread
+    def __init__(self, id, address):
+        Thread.__init__(self)
+        self.id = id
+        self.address = address
+        self.enviados = 0
+        print("Nuevo Thread comenzado por "+ address)
+    #Metodo que ejecuta el thread
+    def run(self):
+        global buf
+        global separador
+        global Verification_code
+        global file_name
+        global sock
+
+        #Envia nombre del archivo y el codigo de verificacion de Hash
+        print("filename_md5::"+file_name+separador+Verification_code)
+        sock.sendto((file_name+separador+Verification_code).encode(),self.address)
+        
+        print ('\nEsperando indicador de inicio del cliente')
+        ack, address = sock.recvfrom(buf)
+        print ('received %s bytes from %s' % (len(ack), address))
+        print (ack)
+        
+        if ack:
+            f=open(file_name,"rb")
+            data = f.read(buf)
+            tInicial = time.time()
+            while (data):
+                sent = sock.sendto(data, address)
+                if(sent):
+                    print ('sent %s bytes back to %s' % (sent, address))
+                    data = f.read(buf)
+                    self.enviados += 1
+            f.close()
+            print ('sent %s bytes back to %s' % (sent, address))
+
+            print ('\nEsperando confirmacion hash y num datagramas recibidos')
+            msg_last, address = sock.recvfrom(buf)
+            tFinal = time.time()
+            print(msg_last)
+            msg_hash, recibidos = msg_last.split(separador)
+            
+            with open(LogTxt, 'w') as log:
+                log.write('Cliente %i - fragmentos enviados: %i' % (self.id, self.enviados) + '\n')
+                log.write('Cliente %i - fragmentos recibidos: %s' % (self.id, recibidos) + '\n')
+                log.write('Cliente %i - verificacion: %s' % (self.id, msg_hash)+ '\n' )
+                log.write('Cliente %i - tInicial: %s seg' % (self.id,str(tInicial)) + '\n')
+                log.write('Cliente %i - tFinal: %s seg' % (self.id, str(tFinal)) + '\n')
+                log.write('Cliente %i - tTotal: %s seg' % (self.id, str(tFinal - tInicial)) + '\n')
+                log.close()
+    
+
+# Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
@@ -47,54 +103,61 @@ def createVerificationCode(filename):
         print("Codigo de verificacion:"+Verification_code)
     return Verification_code
 
+verification_code = createVerificationCode(file_name)
+clientId = 0
+
 while True:
     print ('\nWaiting to receive message')
     data, address = sock.recvfrom(buf)
     print ('received %s bytes from %s' % (len(data), address))
     print (data)
 
+    newthread = ClientThread(ip, clientId, address)
+    newthread.start()
+
+    clientId +=1
     #A partir de aqui va el cliente
     
     
     #Numero de datagramas enviados
-    enviados = 0
+    #enviados = 0
     #Id del cliente
-    clientId = 0
+    #clientId = 0
     
     #Envia nombre del archivo y el codigo de verificacion de Hash
-    print("filename_md5::"+file_name+separador+createVerificationCode(file_name))
-    sock.sendto(file_name+separador+createVerificationCode(file_name).encode(),address)
+    #print("filename_md5::"+file_name+separador+createVerificationCode(file_name))
+    #sock.sendto(file_name+separador+createVerificationCode(file_name).encode(),address)
     
-    print ('\nEsperando indicador de inicio del cliente')
-    ack, address = sock.recvfrom(buf)
-    print ('received %s bytes from %s' % (len(data), address))
-    print (ack)
+    #print ('\nEsperando indicador de inicio del cliente')
+    #ack, address = sock.recvfrom(buf)
+    #print ('received %s bytes from %s' % (len(ack), address))
+    #print (ack)
     
-    if ack:
-        f=open(file_name,"rb")
-        data = f.read(buf)
-        tInicial = time.time()
-        while (data):
-            sent = sock.sendto(data, address)
-            if(sent):
-                print ('sent %s bytes back to %s' % (sent, address))
-                data = f.read(buf)
-                enviados += 1
-        f.close()
-        print ('sent %s bytes back to %s' % (sent, address))
+    #if ack:
+    #    f=open(file_name,"rb")
+    #    data = f.read(buf)
+    #    tInicial = time.time()
+    #    while (data):
+    #        sent = sock.sendto(data, address)
+    #        if(sent):
+    #            print ('sent %s bytes back to %s' % (sent, address))
+    #            data = f.read(buf)
+    #            enviados += 1
+    #    f.close()
+    #    print ('sent %s bytes back to %s' % (sent, address))
 
-        print ('\nEsperando confirmacion hash y num datagramas recibidos')
-        msg_last, address = sock.recvfrom(buf)
-        tFinal = time.time()
-        print(msg_last)
-        msg_hash, recibidos = msg_last.split(separador)
+     #   print ('\nEsperando confirmacion hash y num datagramas recibidos')
+     #   msg_last, address = sock.recvfrom(buf)
+     #   tFinal = time.time()
+     #   print(msg_last)
+     #   msg_hash, recibidos = msg_last.split(separador)
         
-        with open(LogTxt, 'w') as log:
-            log.write('Cliente %i - fragmentos enviados: %i' % (clientId, enviados) + '\n')
-            log.write('Cliente %i - fragmentos recibidos: %s' % (clientId, recibidos) + '\n')
-            log.write('Cliente %i - verificacion: %s' % (clientId, msg_hash)+ '\n' )
-            log.write('Cliente %i - tInicial: %s seg' % (clientId,str(tInicial)) + '\n')
-            log.write('Cliente %i - tFinal: %s seg' % (clientId, str(tFinal)) + '\n')
-            log.write('Cliente %i - tTotal: %s seg' % (clientId, str(tFinal - tInicial)) + '\n')
-            log.close()
+     #   with open(LogTxt, 'w') as log:
+     #       log.write('Cliente %i - fragmentos enviados: %i' % (clientId, enviados) + '\n')
+     #       log.write('Cliente %i - fragmentos recibidos: %s' % (clientId, recibidos) + '\n')
+     #       log.write('Cliente %i - verificacion: %s' % (clientId, msg_hash)+ '\n' )
+     #       log.write('Cliente %i - tInicial: %s seg' % (clientId,str(tInicial)) + '\n')
+     #       log.write('Cliente %i - tFinal: %s seg' % (clientId, str(tFinal)) + '\n')
+     #       log.write('Cliente %i - tTotal: %s seg' % (clientId, str(tFinal - tInicial)) + '\n')
+     #       log.close()
     
